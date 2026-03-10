d3de5c5b3eb37cc82936fc4de7c59d6ae21716a2
Dayy4dev <dafi.ristiansyah@gmail.com>
2026-03-10 10:34:58 +0700

    Menambahkan script yang menampilan nama Dafi

---
+++ Assets/Scripts/AddDafi.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AddDafi : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Dafi added to the project!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
