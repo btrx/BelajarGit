d3872573eb7ad261402f99dac84cb56644ac455d
imtihan200811 <imtihantio200811@gmail.com>
2026-03-10 11:09:54 +0700

    ini adalah nama saya, imtihan

---
+++ Assets/Scripts/ImtihanNama.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ImtihanNama : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("nama saya muhammad Imtihan Ramadhan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
