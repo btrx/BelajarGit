52c90e0a0b3391519b0c0a9f59d0e8867a50a739
janitramahesvari <pitunovj@gmail.com>
2026-03-10 13:29:39 +0700

    Tambah Nama Janitra

--- Assets/Scripts/NamaJanitra.cs
+++ Assets/Scripts/NamaJanitra.cs
@@ -1,11 +1,11 @@
-using UnityEngine;
+giusing UnityEngine;
 
 public class NamaJanitra : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Nama: Janitra Mahesvari Tungga Dewi");
+          Debug.Log("Nama: Janitra Mahesvari Tungga Dewi");
     }
 
     // Update is called once per frame
