1c40b5026f5e8ffc5f2c4592c2904f26b08e516e
mantapujiwa2009 <bing.bramaditya@gmail.com>
2026-03-10 10:55:53 +0700

    Add bing

---
+++ Assets/Scripts/CallNama.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CallNama : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("NamakuBing");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
