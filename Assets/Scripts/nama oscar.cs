0a9ca5886a1e284f661fbda8d75c368a19bceed6
P3trick <yoscar.ap@gmail.com>
2026-03-10 10:43:23 +0700

    Penambahan Nama Oscar

---
+++ Assets/Scripts/nama oscar.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class namaoscar : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Oscar");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
