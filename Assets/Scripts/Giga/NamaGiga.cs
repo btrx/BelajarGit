98b578d08415af34b2d9beda873c11758202f72f
kenzgod110-jpg <kenzgod110@gmail.com>
2026-03-10 15:54:23 +0700

    Selamat Datang

---
+++ Assets/Scripts/Giga/NamaGiga.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NamaGiga : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      Debug.Log("Nama Saya Giga Wahyu Aditya");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
