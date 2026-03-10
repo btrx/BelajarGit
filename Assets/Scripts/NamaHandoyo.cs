3c542ff14f83d3e782367fcef4863d777da8111b
Rizqi Handoyo N. <rizqihandoyo09@gmail.com>
2026-03-10 17:34:22 +0700

    menambahkan nama handoyo

--- Assets/Scripts/NamaHandoyo.cs
+++ Assets/Scripts/NamaHandoyo.cs
@@ -5,7 +5,7 @@ public class NamaHandoyo : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Nama: Rizqi Handoyo Nitimulyo");
+        Debug.Log("Nama saya Rizqi Handoyo Nitimulyo");
     }
 
     // Update is called once per frame
