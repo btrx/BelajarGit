93c917f249b4b4b3e2301d1ad52688a1ab3fcfc2
Jovita <Jovita810@users.noreply.github.com>
2026-03-10 10:28:51 +0700

    menambahkan script nama

---
+++ Assets/Scripts/TambahNamaJovita.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TambahNamaJovita : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Nama saya Jovita");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
