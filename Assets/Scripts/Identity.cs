d1c75849c02c346d8266b3bd832d412b0a9337d2
Neal982 <smkneal1982@gmail.com>
2026-03-10 10:37:57 +0700

    Menambahkan Nama didebug log

---
+++ Assets/Scripts/Identity.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Identity : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("nama saya neal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
