034e0c1b0e2c87d71892b6d77ed4fa8d9db28a61
King-Timothy <jasontimothywibowo@gmail.com>
2026-04-26 15:58:35 +0700

    fix: implement pause and resume state and complete game flow and UI

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
-
-    public GameState currentState;
+     public static GameManager Instance;
+     public GameState currentState;
 
     void Awake()
     {
@@ -14,25 +14,62 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         currentState = GameState.Playing;
+
+        Time.timeScale = 1f;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (currentState == GameState.Playing && Input.GetKeyDown(KeyCode.Escape))
         {
             PauseGame();
+        }
+        else if (currentState == GameState.Paused && Input.GetKeyDown(KeyCode.Escape))
+        {
+            ResumeGame();
+        }
+        else if (currentState == GameState.GameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            RestartGame();
+        }
+        else if (currentState == GameState.GameOver && Input.GetKeyDown(KeyCode.M))
+        {
+            BackToMenu();
         }
     }
 
     public void PauseGame()
     {
         Time.timeScale = 0f;
+
         currentState = GameState.Paused;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+
+        currentState = GameState.Playing;
     }
 
     public void GameOver()
     {
         Debug.Log("Game Over");
         currentState = GameState.GameOver;
+
+        Time.timeScale = 0f;
     }
-}
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+.
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene("MainMenu");
+    }
