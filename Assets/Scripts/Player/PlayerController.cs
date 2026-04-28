ca066d9918a44d07def7d1b25a57563927ed8ada
Dayy4dev <dafi.ristiansyah@gmail.com>
2026-04-28 15:10:04 +0700

    Fix : Connect player to the scriptable object and remove hard code

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,16 +3,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float currentHP = 100;
-    public float speed = 5f;
+    [SerializeField] private PlayerData playerData;
+    private float currentHP;
+    private float speed;
     private PlayerInput playerInput;
     private Vector2 moveInput;
 
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
+
+        if (playerData != null)
+        {
+            currentHP = playerData.maxHP;
+            speed = playerData.moveSpeed;
+        }
+        else
+        {
+            Debug.LogError("PlayerData does not exist, please assign it first!");
+        }
     }
-    
     
     void Update()
     {
@@ -29,7 +39,7 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Wall"))
         {
-            TakeDamage(0.1f);
+            TakeDamage(1f);
         }
     }
 
