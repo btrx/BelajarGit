fcd39900ff425b419fa418b4b47f38f298bea0c8
Jovita <Jovita810@users.noreply.github.com>
2026-04-28 18:47:39 +0700

    scriptable object

--- Assets/Scripts/ScriptableObject/PlayerData.cs
+++ Assets/Scripts/ScriptableObject/PlayerData.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "PlayerData", menuName = "Game/Player Data")]
+[CreateAssetMenu(fileName = "PlayerData", menuName = "Game Data/Player/PlayerData")]
 public class PlayerData : ScriptableObject
 {
+    [Header("Player Stats")]
     public float maxHP;
     public float moveSpeed;
 }
+
+// [CreateAssetMenu(fileName = "PlayerData", menuName = "Game/Player Data")]
+// public class PlayerData : ScriptableObject
+// {
+//     public float maxHP;
+//     public float moveSpeed;
+// }
