8bb36a656a72387976e6fe2ed9a1969739521f8f
adistarakapramudya10-ops <adistarakapramudya10@gmail.com>
2026-04-29 21:10:59 +0700

    fix: connect player to scriptable object

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,10 +1,12 @@
-using UnityEngine;
+gitusing UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
     public GameState currentState;
+
+    public GameObject gameOverPanel; 
 
     void Awake()
     {
@@ -14,6 +16,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         currentState = GameState.Playing;
+        Time.timeScale = 1f; 
     }
 
     void Update()
@@ -33,6 +36,14 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         Debug.Log("Game Over");
+
         currentState = GameState.GameOver;
+
+        Time.timeScale = 0f; 
+
+        if (gameOverPanel != null)
+        {
+            gameOverPanel.SetActive(true); 
+        }
     }
 }
