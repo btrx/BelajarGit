9919a2b908ca6287d57c37ac2606d714a77bcb6c
adityasalman677 <adityasalman677@gmail.com>
2026-04-29 14:45:31 +0700

    fix : connect player to scriptable aboject

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-
     public GameState currentState;
 
     void Awake()
@@ -13,26 +12,57 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        currentState = GameState.Playing;
+        Time.timeScale = 1f;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            if (currentState == GameState.Playing)
+            {
+                PauseGame();
+            }
+            else if (currentState == GameState.Paused)
+            {
+                ResumeGame();
+            }
             PauseGame();
         }
     }
 
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
+        currentState = GameState.Playing;
+    }
     public void PauseGame()
     {
         Time.timeScale = 0f;
         currentState = GameState.Paused;
+        Debug.Log("Game paused");
     }
+
+    public void ResumeGame()
+    {
+        Time.timeScale =1f;
+        currentState = GameState.Playing;
+        Debug.Log("Game Resumed");
+
+    }
+
 
     public void GameOver()
     {
+        Time.timeScale = 0f;
         Debug.Log("Game Over");
         currentState = GameState.GameOver;
+       
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        currentState = GameState.Playing;
     }
 }
