d8f724eab8e79fbe0a6c97f3f1682ceaa0863c28
Dzaki988 <dzakiabdurrohman29@gmail.com>
2026-04-30 09:00:17 +0700

    fix: connect player to scriptable object

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,44 +3,61 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float currentHP = 100;
-    public float speed = 5f;
+    [Header("Penghubung Ke Data")]
+    [SerializeField] private PlayerData PlayerData;
+
+    private float currentHP;
+
+
     private PlayerInput playerInput;
     private Vector2 moveInput;
 
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
+        currentHP = PlayerData.maxHP;
     }
     
     
     void Update()
-    {
-        if (playerInput == null) return;
-        
-        moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
-        float h = moveInput.x;
-        float v = moveInput.y;
+{
+    if (playerInput == null) return;
+    
+    moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+    
+    if (moveInput != Vector2.zero) Debug.Log("Input Terdeteksi: " + moveInput);
 
-        transform.Translate(new Vector3(h, v, 0) * speed * Time.deltaTime);
-    }
+    float h = moveInput.x;
+    float v = moveInput.y;
+
+    if (PlayerData == null) return;
+    transform.Translate(new Vector3(h, v, 0) * PlayerData.moveSpeed * Time.deltaTime);
+}
 
     void OnCollisionStay2D(Collision2D collision)
+{
+    if (PlayerData != null && collision.gameObject.CompareTag("Wall"))
     {
-        if (collision.gameObject.CompareTag("Wall"))
-        {
-            TakeDamage(0.1f);
-        }
+        TakeDamage(PlayerData.damageTaken);
     }
+}
 
-    void TakeDamage(float dmg)
+ void TakeDamage(float dmg)
+{
+    currentHP -= dmg;
+    Debug.Log("Player HP: " + currentHP);
+
+    if (currentHP <= 0)
     {
-        currentHP -= dmg;
-        Debug.Log("Player HP: " + currentHP);
-
-        if (currentHP <= 0)
+        // Cek apakah GameManager sudah ada di Scene
+        if (GameManager.Instance != null)
         {
             GameManager.Instance.GameOver();
         }
+        else
+        {
+            Debug.LogError("Waduh! Kamu lupa pasang script GameManager di Hierarchy!");
+        }
     }
+}
 }
