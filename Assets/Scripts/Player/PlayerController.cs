8d11bc4caca9c5e57baebf2fea04cb54fae0baab
farrahkeisha-sart <farrahkeisha59@gmail.com>
2026-05-09 17:57:16 +0700

    fix: connect player to scriptable object

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerData playerData;
+
+    public float currentHP;
+    public float speed;
+
+    private PlayerInput playerInput;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+
+        currentHP = playerData.maxHP;
+        speed = playerData.moveSpeed;
+    }
+
+    void Update()
+    {
+        if (playerInput == null) return;
+
+        moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+
+        float h = moveInput.x;
+        float v = moveInput.y;
+
+        transform.Translate(new Vector3(h, v, 0) * speed * Time.deltaTime);
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            TakeDamage(0.1f);
+        }
+    }
+
+    void TakeDamage(float dmg)
+    {
+        currentHP -= dmg;
+
+        Debug.Log("Player HP: " + currentHP);
+
+        if (currentHP <= 0)
+        {
+            GameManager.Instance.GameOver();
+        }
+    }
+}
