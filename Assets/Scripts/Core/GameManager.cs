cfb1af1d1f306ec9a493f40167c9fad40dcb0180
rabbanirakha8-web <rabbanirakha8@gmail.com>
2026-05-09 23:07:11 +0700

    fitur: implementasi logika game state dan pause

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -13,26 +13,64 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        currentState = GameState.Playing;
+        // biar play dari main menu
+        ChangeState(GameState.MainMenu);
     }
 
     void Update()
     {
+        // Logika Pause menggunakan Escape
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            PauseGame();
+            if (currentState == GameState.Playing)
+            {
+                PauseGame();
+            }
+            else if (currentState == GameState.Paused)
+            {
+                ResumeGame();
+            }
+        }
+    }
+
+    // Fungsi tambahan biar gonta ganti lah pokoknya
+    public void ChangeState(GameState newState)
+    {
+        currentState = newState;
+        
+        switch (newState)
+        {
+            case GameState.MainMenu:
+                Time.timeScale = 1f;
+                break;
+            case GameState.Playing:
+                Time.timeScale = 1f;
+                break;
+            case GameState.Paused:
+                Time.timeScale = 0f;
+                break;
+            case GameState.GameOver:
+                Time.timeScale = 0f; //  Waktu berhenti saat kalah
+                break;
         }
     }
 
     public void PauseGame()
     {
-        Time.timeScale = 0f;
-        currentState = GameState.Paused;
+        ChangeState(GameState.Paused);
+    }
+
+    // Fungsi tambahan agar player bisa main lagi setelah pause
+    public void ResumeGame()
+    {
+        ChangeState(GameState.Playing);
     }
 
     public void GameOver()
     {
         Debug.Log("Game Over");
-        currentState = GameState.GameOver;
+        ChangeState(GameState.GameOver);
     }
 }
+
+// udahan ah, besok ajah 
