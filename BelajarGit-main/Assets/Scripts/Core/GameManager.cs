81d2a1b3eb2256778317dc67d0c59a6bac998873
ayrafatmatuzzahra23-source <ayrafatmatuzzahra23@gmail.com>
2026-05-11 00:24:20 +0800

    fix: final update game flow and gameplay adjustments

--- BelajarGit-main/Assets/Scripts/Core/GameManager.cs
+++ BelajarGit-main/Assets/Scripts/Core/GameManager.cs
@@ -14,7 +14,39 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-    ChangeState(GameState.Playing);
+        ChangeState(GameState.Playing);
+    }
+
+    void Update()
+    {
+       
+        if (currentState == GameState.MainMenu)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                LoadGame();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (currentState == GameState.Playing)
+            {
+                PauseGame();
+            }
+            else if (currentState == GameState.Paused)
+            {
+                ResumeGame();
+            }
+        }
+
+        if (currentState == GameState.GameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                RestartGame();
+            }
+        }
     }
 
     public void ChangeState(GameState newState)
@@ -25,7 +57,7 @@ public class GameManager : MonoBehaviour
         {
             case GameState.MainMenu:
                 Time.timeScale = 0;
-                Debug.Log("Main Menu");
+                Debug.Log("Main Menu - Press SPACE to Start");
                 break;
 
             case GameState.Playing:
@@ -35,19 +67,14 @@ public class GameManager : MonoBehaviour
 
             case GameState.Paused:
                 Time.timeScale = 0;
-                Debug.Log("Pause");
+                Debug.Log("Paused - Press P to Resume");
                 break;
 
             case GameState.GameOver:
                 Time.timeScale = 0;
-                Debug.Log("Game Over");
+                Debug.Log("Game Over - Press R to Restart");
                 break;
         }
-    }
-
-    public void StartGame()
-    {
-        ChangeState(GameState.Playing);
     }
 
     public void PauseGame()
@@ -72,5 +99,11 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(
             SceneManager.GetActiveScene().buildIndex
         );
+    }
+
+    public void LoadGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Game");
     }
 }
