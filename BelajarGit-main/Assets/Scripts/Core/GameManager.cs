5e5f8c20a0b7c1c30bb2d691313d0c120a7d59cc
ayrafatmatuzzahra23-source <ayrafatmatuzzahra23@gmail.com>
2026-05-10 21:18:42 +0800

    fix: implement scriptable object and game state

---
+++ BelajarGit-main/Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public GameState currentState;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        ChangeState(GameState.MainMenu);
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        currentState = newState;
+
+        switch (newState)
+        {
+            case GameState.MainMenu:
+                Time.timeScale = 0;
+                Debug.Log("Main Menu");
+                break;
+
+            case GameState.Playing:
+                Time.timeScale = 1;
+                Debug.Log("Playing");
+                break;
+
+            case GameState.Paused:
+                Time.timeScale = 0;
+                Debug.Log("Pause");
+                break;
+
+            case GameState.GameOver:
+                Time.timeScale = 0;
+                Debug.Log("Game Over");
+                break;
+        }
+    }
+
+    public void StartGame()
+    {
+        ChangeState(GameState.Playing);
+    }
+
+    public void PauseGame()
+    {
+        ChangeState(GameState.Paused);
+    }
+
+    public void ResumeGame()
+    {
+        ChangeState(GameState.Playing);
+    }
+
+    public void GameOver()
+    {
+        ChangeState(GameState.GameOver);
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+
+        SceneManager.LoadScene(
+            SceneManager.GetActiveScene().buildIndex
+        );
+    }
+}
