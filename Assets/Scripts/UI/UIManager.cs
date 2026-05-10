d92183a20511767b12c34f4873ccaeb4f7e7260f
rabbanirakha8-web <rabbanirakha8@gmail.com>
2026-05-10 11:03:27 +0700

    fitur: menyelesaikan alur game flow dan integrasi UI

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -5,16 +5,34 @@ public class UIManager : MonoBehaviour
 {
     public void StartGame()
     {
+        // Pindah ke scene game
         SceneManager.LoadScene("Game");
+        
+        
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.ChangeState(GameState.Playing);
+        }
     }
 
     public void QuitGame()
     {
         Application.Quit();
+        Debug.Log("Game ditutup"); 
     }
 
     public void Restart()
     {
-        SceneManager.LoadScene("Game");
+        
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Resume()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.ResumeGame();
+        }
     }
 }
