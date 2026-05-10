eb923f092e58673a76b6fb0a976e23e935446963
raffaalghifari490-pixel <raffaalghifari490@gmail.com>
2026-05-10 19:33:40 +0700

    fix: scriptable object player data

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-
     public GameState currentState;
+    public GameObject gameOverPanel;
+    public GameObject pausePanel;
 
     void Awake()
     {
@@ -14,13 +16,18 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         currentState = GameState.Playing;
+        gameOverPanel.SetActive(false);
+        pausePanel.SetActive(false);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            PauseGame();
+            if (currentState == GameState.Playing)
+                PauseGame();
+            else if (currentState == GameState.Paused)
+                ResumeGame();
         }
     }
 
@@ -28,11 +35,28 @@ public class GameManager : MonoBehaviour
     {
         Time.timeScale = 0f;
         currentState = GameState.Paused;
+        pausePanel.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        currentState = GameState.Playing;
+        pausePanel.SetActive(false);
     }
 
     public void GameOver()
     {
         Debug.Log("Game Over");
+        Time.timeScale = 0f;
         currentState = GameState.GameOver;
+        gameOverPanel.SetActive(true);
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        currentState = GameState.MainMenu;
+        SceneManager.LoadScene("MainMenu");
     }
 }
