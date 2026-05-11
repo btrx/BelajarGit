0d88791e91e52b992b50814317f857a7c7a84026
MuhammadRizkyRamdhani <sugiyanto2790@gmail.com>
2026-05-11 10:28:50 +0700

    fiks : game state and game flow

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,20 +1,65 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public void StartGame()
-    {
-        SceneManager.LoadScene("Game");
-    }
+    public GameObject pausePanel; 
+    public GameObject gameOverPanel;
 
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
+        
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
 
-    public void Restart()
-    {
-        SceneManager.LoadScene("Game");
-    }
+        SceneManager.LoadScene("Game");
+    }
+
+    public void PauseGame()
+    {
+        GameManager.Instance.currentState = GameState.Paused;
+
+        Time.timeScale = 0f;
+
+        pausePanel.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        GameManager.Instance.currentState = GameState.Playing;
+
+        Time.timeScale = 1f;
+
+        pausePanel.SetActive(false);
+    }
+
+    public void ShowGameOver()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        if (Application.isEditor)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
 }
