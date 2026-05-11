755da1104f2437a5b24e94d5892409c16504b655
Azmi12-spec <azmishofamuhammad@gmail.com>
2026-05-11 15:00:03 +0700

    fix code hardcode playercontroller and error code game manager,game state,playerdata,uimanager

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,35 +3,32 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public PlayerData playerData;
-
-    private float currentHP;
+    [SerializeField] private PlayerData data; 
+    
+    public float currentHP;
     private float speed;
-
     private PlayerInput playerInput;
     private Vector2 moveInput;
 
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
-
-        currentHP = playerData.maxHP;
-        speed = playerData.moveSpeed;
+        
+        if (data != null)
+        {
+            currentHP = data.maxHP;
+            speed = data.moveSpeed;
+        }
     }
-
+    
     void Update()
     {
-        if (GameManager.Instance.currentState != GameState.Playing)
-            return;
-
-        if (playerInput == null) return;
-
-        moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
-
-        float h = moveInput.x;
-        float v = moveInput.y;
-
-        transform.Translate(new Vector3(h, v, 0) * speed * Time.deltaTime);
+    if (GameManager.Instance == null || GameManager.Instance.currentState != GameState.Playing) return;
+    
+    if (playerInput == null) return;
+    
+    moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+    transform.Translate(new Vector3(moveInput.x, moveInput.y, 0) * speed * Time.deltaTime);
     }
 
     void OnCollisionStay2D(Collision2D collision)
@@ -45,12 +42,11 @@ public class PlayerController : MonoBehaviour
     void TakeDamage(float dmg)
     {
         currentHP -= dmg;
-
         Debug.Log("Player HP: " + currentHP);
 
-        if (currentHP <= 0)
-        {
-            GameManager.Instance.GameOver();
-        }
+    if (currentHP <= 0)
+    {
+        GameManager.Instance.GameOver(); 
+    }
     }
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,18 +3,43 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Panels")]
+    public GameObject pausePanel;
+    public GameObject gameOverPanel;
+
+    void Update()
+    {
+        if (GameManager.Instance == null) return;
+
+        if (pausePanel != null)
+            pausePanel.SetActive(GameManager.Instance.currentState == GameState.Paused);
+            
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(GameManager.Instance.currentState == GameState.GameOver);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Game");
     }
 
-    public void QuitGame()
+    public void Resume()
     {
-        Application.Quit();
+        GameManager.Instance.ResumeGame();
     }
 
     public void Restart()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
