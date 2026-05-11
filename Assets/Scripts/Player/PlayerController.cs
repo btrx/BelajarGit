ca37a3cda361c3f12a1bbdb8a2803aaeff521bb8
MuhammadRizkyRamdhani <sugiyanto2790@gmail.com>
2026-05-11 11:46:57 +0700

    fiks : player control

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,44 +3,47 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float currentHP = 100;
-    public float speed = 5f;
-    private PlayerInput playerInput;
-    private Vector2 moveInput;
+    public float currentHP; 
+    public float speed;
+    
+    private PlayerInput playerInput;
+    private Vector2 moveInput;
 
-    void Start()
-    {
-        playerInput = GetComponent<PlayerInput>();
-    }
-    
-    
-    void Update()
-    {
-        if (playerInput == null) return;
-        
-        moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
-        float h = moveInput.x;
-        float v = moveInput.y;
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
+    
+    void Update()
+    {
+        if (playerInput == null) return;
+        
+        moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+        float h = moveInput.x;
+        float v = moveInput.y;
 
-        transform.Translate(new Vector3(h, v, 0) * speed * Time.deltaTime);
-    }
+        transform.Translate(new Vector3(h, v, 0) * speed * Time.deltaTime);
+    }
 
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Wall"))
-        {
-            TakeDamage(0.1f);
-        }
-    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            TakeDamage(1f);
+        }
+    }
 
-    void TakeDamage(float dmg)
-    {
-        currentHP -= dmg;
-        Debug.Log("Player HP: " + currentHP);
+    void TakeDamage(float dmg)
+    {
+        currentHP -= dmg;
+        Debug.Log("Player HP: " + currentHP);
 
-        if (currentHP <= 0)
-        {
-            GameManager.Instance.GameOver();
-        }
-    }
+        if (currentHP <= 0)
+        {
+            if (GameManager.Instance != null)
+            {
+                GameManager.Instance.GameOver();
+            }
+        }
+    }
 }
