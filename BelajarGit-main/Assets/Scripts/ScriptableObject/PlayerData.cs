d799ccf8ff3b114949effc2a0f4917eb518c5278
ayrafatmatuzzahra23-source <ayrafatmatuzzahra23@gmail.com>
2026-06-06 09:11:54 +0800

    Menambahkan fitur attack dan object pooling

--- BelajarGit-main/Assets/Scripts/ScriptableObject/PlayerData.cs
+++ BelajarGit-main/Assets/Scripts/ScriptableObject/PlayerData.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerData", menuName = "Game/Player Data")]
 public class PlayerData : ScriptableObject
 {
-    public float hp = 100;
-    public float speed = 5f;
+    public float maxHP;
+    public float moveSpeed;
 }
