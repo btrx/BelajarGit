80631ab22b4139c4b532e70e547e6c1996dc514c
gigaabhiseka-ai <gigaabhiseka@gmail.com>
2026-06-07 19:32:45 +0700

    melakukan commit tiga

---
+++ Assets/Scripts/Core/Projectile.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BelajarGit.Assets.Scripts.Core
+{
+    public class Projectile
+    {
+        
+    }
+}
