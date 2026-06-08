8630416b18b85afc4f6f6582e69708f4a92b12f2
reysigma69 <reynandasigma69@gmail.com>
2026-06-08 21:40:30 +0700

    memindahkan project unity dari branch uts-fix ke branch uts-fix-baru untuk memunculkan opsi pull request

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
     public GameState currentState;
+    public GameObject pausePanel;
+    public GameObject gameOverPanel;
 
     void Awake()
     {
-        Instance = this;
+        if (Instance == null) Instance = this;
     }
 
     void Start()
@@ -31,12 +35,36 @@ public class GameManager : MonoBehaviour
     public void PauseGame()
     {
         Time.timeScale = 0f;
+        if(pausePanel != null) pausePanel.SetActive(true);
         currentState = GameState.Paused;
+        Debug.Log("Game Paused");
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        if(pausePanel != null) pausePanel.SetActive(false);
+        currentState = GameState.Playing;
+        Debug.Log("Game Resumed");
     }
 
     public void GameOver()
     {
         Debug.Log("Game Over");
+        Time.timeScale = 0f;
+        if(gameOverPanel != null) gameOverPanel.SetActive(true);
         currentState = GameState.GameOver;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log("Game Restarted");
+    }
+
+    public void LoadMainMenu()
+    {
+        Debug.Log("gas pencet play");
+        SceneManager.LoadScene("MainMenu");
     }
 }
